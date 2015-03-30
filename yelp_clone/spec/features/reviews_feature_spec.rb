c266a05e67940b5565ffd30b3a55700054aab417
wardymate <chriswarddev@gmail.com>
2015-03-30 17:51:12 +0100

    added test to check review is deleted when restaurant is deleted - all tests pass

--- yelp_clone/spec/features/reviews_feature_spec.rb
+++ yelp_clone/spec/features/reviews_feature_spec.rb
@@ -3,15 +3,25 @@ require 'rails_helper'
 feature 'reviewing' do
   before {Restaurant.create name: 'KFC'}
 
-  scenario 'allows users to leave a review using a form' do
-     visit '/restaurants'
+  def add_review
+    visit '/restaurants'
      click_link 'Review KFC'
      fill_in "Thoughts", with: "so so"
      select '3', from: 'Rating'
      click_button 'Leave Review'
-
      expect(current_path).to eq '/restaurants'
      expect(page).to have_content('so so')
+  end
+
+  scenario 'allows users to leave a review using a form' do
+     add_review
+  end
+
+  scenario 'deletes a review when a restaurant is deleted' do
+    add_review
+    click_link "Delete KFC"
+    expect(page).not_to have_content 'KFC'
+    expect(page).to have_content 'Restaurant deleted successfully'
   end
 
 end
