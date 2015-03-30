a053b25833b037a56a7cb2e3f08804f96ae4c2c4
wardymate <chriswarddev@gmail.com>
2015-03-30 15:37:32 +0100

    can now add restaurant and view details of restaurant, all tests pass

--- yelp_clone/app/controllers/restaurants_controller.rb
+++ yelp_clone/app/controllers/restaurants_controller.rb
@@ -1,19 +1,24 @@
 class RestaurantsController < ApplicationController
+
   def index
     @restaurants = Restaurant.all
   end
 
   def new
-
+    @restaurant = Restaurant.new
   end
 
   def create
-    Restaurant.create(restaurant_params)
+    @restaurant = Restaurant.create(restaurant_params)
     redirect_to '/restaurants'
   end
 
   def restaurant_params
     params.require(:restaurant).permit(:name)
+  end
+
+  def show
+    @restaurant = Restaurant.find(params[:id])
   end
 
 end
