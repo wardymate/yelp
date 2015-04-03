4f1d086f3ebb82fc5c4c71611bd3f075c4380c8a
wardymate <chriswarddev@gmail.com>
2015-04-03 18:46:56 +0100

    can upload pictures

--- yelp_clone/app/models/restaurant.rb
+++ yelp_clone/app/models/restaurant.rb
@@ -1,4 +1,7 @@
 class Restaurant < ActiveRecord::Base
+  has_attached_file :image, :styles => { :medium => "300x300>", :thumb => "100x100>" }, :default_url => "https://s3-us-west-2.amazonaws.com/yelp-clone-makers/restaurants/images/missing.png"
+  validates_attachment_content_type :image, :content_type => /\Aimage\/.*\Z/
+
   belongs_to :user
   has_many :reviews, dependent: :destroy
   validates :name, length: {minimum: 3}, uniqueness: true
